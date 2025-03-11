1cec43630e4da6468cf8e2e1e31a6ceaa297cb42
izabella21 <izabellaferracioli798@gmail.com>
2025-03-11 01:31:42 +0000

    Criei o primeiro passo na construção do seu jogo de Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
@@ -35,6 +35,86 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0;  
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+bool verificarPosicaoValida(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int orientacao) {
+    if (orientacao == 0) {  
+        if (coluna + TAMANHO_NAVIO > TAMANHO_TABULEIRO) return false;  
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha][coluna + i] != 0) return false;  
+        }
+    } else {  
+        if (linha + TAMANHO_NAVIO > TAMANHO_TABULEIRO) return false;  
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha + i][coluna] != 0) return false;  
+        }
+    }
+    return true;
+}
+
+void posicionarNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int orientacao) {
+    if (orientacao == 0) {  // Horizontal
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[linha][coluna + i] = 3;  
+        }
+    } else {  // Vertical
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            tabuleiro[linha + i][coluna] = 3;  
+        }
+    }
+}
+
+int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];  
+    
+    inicializarTabuleiro(tabuleiro);
+
+    
+    int linhaHorizontal = 2, colunaHorizontal = 1;  
+    int linhaVertical = 4, colunaVertical = 6;      
+
+    
+    if (verificarPosicaoValida(tabuleiro, linhaHorizontal, colunaHorizontal, 0)) {
+        posicionarNavio(tabuleiro, linhaHorizontal, colunaHorizontal, 0);
+    } else {
+        printf("Posição invalida para o navio horizontal!\n");
+    }
+
+    
+    if (verificarPosicaoValida(tabuleiro, linhaVertical, colunaVertical, 1)) {
+        posicionarNavio(tabuleiro, linhaVertical, colunaVertical, 1);
+    } else {
+        printf("Posição invalida para o navio vertical!\n");
+    }
+
+    printf("Tabuleiro de Batalha Naval:\n");
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
